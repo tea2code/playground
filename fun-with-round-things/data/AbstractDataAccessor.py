69a50d4d1ed35dca71247944afd8fc9d107964b5
tea2code <stefan4875@gmail.com>
2012-12-30 15:08:38 +0100

    Removed data accessor.

--- fun-with-round-things/data/AbstractDataAccessor.py
+++
@@ -1,7 +0,0 @@
-""" This abstract class defines a methods to get access to the global data object. Must be implemented
-by every data accessor.
-
-"""
-class AbstractDataAccessor:
-    data = None
-    
