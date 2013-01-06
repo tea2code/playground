075bcc024f74dbcbc03eedcdb0119506b523710a
tea2code <stefan4875@gmail.com>
2013-01-06 11:08:11 +0100

    String method and tests for MoveState. Closes #13. Closes #14. Closes #15.

--- fun-with-round-things/physics/movestate.py
+++ fun-with-round-things/physics/movestate.py
@@ -5,13 +5,47 @@ class MoveState:
     v = 0
 
     def __add__( self, other ):
+        """ Sum up two move states using +. Corresponds to addition of vectors.
+        
+        Test:
+        >>> m1 = MoveState()
+        >>> m1.x = 1
+        >>> m1.v = 2
+        >>> m2 = MoveState()
+        >>> m2.x = 3
+        >>> m2.v = 4
+        >>> print(m1 + m2)
+        MoveState(x 4.00, v 6.00)
+        """
         new = MoveState()
         new.x = self.x + other.x
         new.v = self.v + other.v
         return new
     
     def __mul__( self, scalar ):
+        """ Multiplicate object with scalar using *. Corresponds to scalar multiplication of vectors.
+        
+        Test:
+        >>> m = MoveState()
+        >>> m.x = 1
+        >>> m.v = 2
+        >>> print(m * 5)
+        MoveState(x 5.00, v 10.00)
+        """
         new = MoveState()
         new.x = self.x * scalar
         new.v = self.v * scalar
         return new
+        
+    def __str__( self ):
+        """ Test:
+        >>> m = MoveState()
+        >>> print(m)
+        MoveState(x 0.00, v 0.00)
+        """
+        return 'MoveState(x {:.2f}, v {:.2f})'.format(self.x, self.v)
+        
+if __name__ == '__main__':
+    print( 'Executing doctest.' )
+    import doctest
+    doctest.testmod()
--- fun-with-round-things/test.py
+++ fun-with-round-things/test.py
@@ -4,6 +4,7 @@ import data.circle
 import data.movable
 import fps.fps
 import fps.fpscounter
+import physics.movestate
 
 if __name__ == '__main__':
     import doctest
@@ -13,3 +14,4 @@ if __name__ == '__main__':
     doctest.testmod( data.movable )
     doctest.testmod( fps.fps )
     doctest.testmod( fps.fpscounter )
+    doctest.testmod( physics.movestate )
