9798fe787984d0710bc0a895eb4f2caae760848b
tea2code <stefan4875@gmail.com>
2014-03-13 00:09:01 +0100

    Implemented iterator interface and derived iterator.

--- Inheritance/Inheritance.pro
+++ Inheritance/Inheritance.pro
@@ -5,7 +5,7 @@ CONFIG -= qt
 
 DEFINES += BOOST_RESULT_OF_USE_DECLTYPE
 
-INCLUDEPATH += $$PWD/../../boost_1_55_0
+INCLUDEPATH += $$PWD/../../libraries/boost_1_55_0
 
 SOURCES += main.cpp
 
@@ -16,4 +16,4 @@ QMAKE_CXXFLAGS += -std=c++11
 QMAKE_CXXFLAGS += -pedantic -Wall -Wextra -Werror
 
 # Deactivate some unnecessary warnings. Best to use "-Wno-error=..." and keep warnings.
-QMAKE_CXXFLAGS += -Wno-error=unused-parameter
+QMAKE_CXXFLAGS += -Wno-error=unused-parameter -Wno-error=return-type
--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -1,53 +1,97 @@
 #include <boost/iterator/transform_iterator.hpp>
 
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
 
-template<class Iterator>
+class BaseIterator;
+
 class Base
 {
 public:
-    virtual Iterator begin() = 0;
-    virtual Iterator end() = 0;
+    virtual BaseIterator* begin() = 0;
+    virtual BaseIterator* end() = 0;
+
+    virtual std::string name() = 0;
 };
 
-struct toString
-{
-    std::string operator()(int number) const
-    {
-        return std::to_string(number);
-    }
-};
-
-typedef boost::transform_iterator<toString, std::vector<int>::iterator> TransformIterator;
-
-class Derived : public Base<TransformIterator>
+class BaseIterator
 {
 public:
-    Derived() : ints{132, 3, 6451, 12, 5} {}
+    typedef BaseIterator self_type;
+    typedef Base value_type;
+    typedef Base& reference;
+    typedef Base* pointer;
+    typedef std::forward_iterator_tag iterator_category;
+    typedef int difference_type;
 
-    TransformIterator begin()
-    {
-        return boost::make_transform_iterator(ints.begin(), toString());
-    }
+    virtual self_type* operator++() = 0;
+    virtual self_type* operator++(int junk) = 0;
+    virtual reference operator*() = 0;
+    virtual pointer operator->() = 0;
+    virtual bool operator==(self_type* rhs) = 0;
+    virtual bool operator!=( self_type* rhs) = 0;
+};
 
-    TransformIterator end()
-    {
-        return boost::make_transform_iterator(ints.end(), toString());
-    }
+class Derived;
+
+class DerivedIterator : public BaseIterator
+{
+public:
+    DerivedIterator(std::vector<Derived*>::iterator iterator) : iterator(iterator) { }
+    self_type* operator++() { self_type* i = new DerivedIterator(iterator); iterator++; return i; }
+    self_type* operator++(int junk) { iterator++; return new DerivedIterator(iterator); }
+    reference operator*();
+    pointer operator->();
+    bool operator==(self_type* rhs);
+    bool operator!=(self_type* rhs);
 
 private:
-    std::vector<int> ints;
+    std::vector<Derived*>::iterator iterator;
 };
+
+class Derived : public Base
+{
+public:
+    Derived(std::string name) : n(name) {}
+
+    BaseIterator* begin()
+    {
+        return new DerivedIterator(v.begin());
+    }
+
+    BaseIterator* end()
+    {
+        return new DerivedIterator(v.end());
+    }
+
+    std::string name()
+    {
+        return n;
+    }
+
+    std::vector<Derived*> v;
+    std::string n;
+};
+
+BaseIterator::reference DerivedIterator::operator*() { return *(*iterator); }
+BaseIterator::pointer DerivedIterator::operator->() { return *iterator; }
+bool DerivedIterator::operator==(BaseIterator::self_type* rhs) { return iterator == dynamic_cast<DerivedIterator*>(rhs)->iterator; }
+bool DerivedIterator::operator!=(BaseIterator::self_type* rhs) { return iterator != dynamic_cast<DerivedIterator*>(rhs)->iterator; }
 
 int main()
 {
-    Base<TransformIterator>* obj = new Derived();
-    for(const auto& value : *obj)
+    Derived* d = new Derived("1");
+    d->v.push_back(new Derived("2"));
+    d->v.push_back(new Derived("3"));
+
+    Base* obj = d;
+    for(auto& value : *obj)
     {
-        std::cout << value.size() << std::endl;
+        std::cout << value->name() << std::endl;
     }
+
     return 0;
 }
 
