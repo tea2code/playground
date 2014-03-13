20c078f6c1bb3deceafe537ac7416a5637e67122
tea2code <stefan4875@gmail.com>
2014-03-13 13:00:58 +0100

    It works \o/

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -7,11 +7,35 @@
 
 class BaseIterator;
 
+template<class IteratorImpl, class Value>
+class Iterator
+{
+public:
+    typedef Iterator self_type;
+    typedef Value value_type;
+    typedef Value& reference;
+    typedef Value* pointer;
+    typedef std::forward_iterator_tag iterator_category;
+    typedef int difference_type;
+
+    Iterator(IteratorImpl *iterator) : iterator(iterator) {}
+
+    self_type operator++() { self_type i = *this; (*iterator)++; return i; }
+    self_type operator++(int junk) { (*iterator)++; return *this; }
+    reference operator*() { return *(*iterator); }
+    pointer operator->() { return &(*(*iterator)); }
+    bool operator==(const self_type& rhs) { return *iterator == *rhs.iterator; }
+    bool operator!=(const self_type& rhs) { return *iterator != *rhs.iterator; }
+
+private:
+    IteratorImpl *iterator;
+};
+
 class Base
 {
 public:
-    virtual BaseIterator* begin() = 0;
-    virtual BaseIterator* end() = 0;
+    virtual Iterator<BaseIterator, Base> begin() = 0;
+    virtual Iterator<BaseIterator, Base> end() = 0;
 
     virtual std::string name() = 0;
 };
@@ -30,8 +54,8 @@ public:
     virtual self_type* operator++(int junk) = 0;
     virtual reference operator*() = 0;
     virtual pointer operator->() = 0;
-    virtual bool operator==(self_type* rhs) = 0;
-    virtual bool operator!=( self_type* rhs) = 0;
+    virtual bool operator==(const self_type& rhs) = 0;
+    virtual bool operator!=(const self_type& rhs) = 0;
 };
 
 class Derived;
@@ -39,13 +63,14 @@ class Derived;
 class DerivedIterator : public BaseIterator
 {
 public:
-    DerivedIterator(std::vector<Derived*>::iterator iterator) : iterator(iterator) { }
-    self_type* operator++() { self_type* i = new DerivedIterator(iterator); iterator++; return i; }
-    self_type* operator++(int junk) { iterator++; return new DerivedIterator(iterator); }
+    DerivedIterator(std::vector<Derived*>::iterator iterator) : iterator(iterator) {}
+
+    self_type* operator++();
+    self_type* operator++(int junk);
     reference operator*();
     pointer operator->();
-    bool operator==(self_type* rhs);
-    bool operator!=(self_type* rhs);
+    bool operator==(const self_type& rhs);
+    bool operator!=(const self_type& rhs);
 
 private:
     std::vector<Derived*>::iterator iterator;
@@ -56,14 +81,14 @@ class Derived : public Base
 public:
     Derived(std::string name) : n(name) {}
 
-    BaseIterator* begin()
+    Iterator<BaseIterator, Base> begin()
     {
-        return new DerivedIterator(v.begin());
+        return Iterator<BaseIterator, Base>(new DerivedIterator(v.begin()));
     }
 
-    BaseIterator* end()
+    Iterator<BaseIterator, Base> end()
     {
-        return new DerivedIterator(v.end());
+        return Iterator<BaseIterator, Base>(new DerivedIterator(v.end()));
     }
 
     std::string name()
@@ -75,22 +100,28 @@ public:
     std::string n;
 };
 
-BaseIterator::reference DerivedIterator::operator*() { return *(*iterator); }
-BaseIterator::pointer DerivedIterator::operator->() { return *iterator; }
-bool DerivedIterator::operator==(BaseIterator::self_type* rhs) { return iterator == dynamic_cast<DerivedIterator*>(rhs)->iterator; }
-bool DerivedIterator::operator!=(BaseIterator::self_type* rhs) { return iterator != dynamic_cast<DerivedIterator*>(rhs)->iterator; }
+DerivedIterator::self_type* DerivedIterator::operator++() { self_type* i = this; iterator++; return i; }
+DerivedIterator::self_type* DerivedIterator::operator++(int) { iterator++; return this; }
+DerivedIterator::reference DerivedIterator::operator*() { return *(*iterator); }
+DerivedIterator::pointer DerivedIterator::operator->() { return *iterator; }
+bool DerivedIterator::operator==(const BaseIterator::self_type& rhs) { return iterator == dynamic_cast<const DerivedIterator&>(rhs).iterator; }
+bool DerivedIterator::operator!=(const BaseIterator::self_type& rhs) { return iterator != dynamic_cast<const DerivedIterator&>(rhs).iterator; }
 
 int main()
 {
-    Derived* d = new Derived("1");
-    d->v.push_back(new Derived("2"));
-    d->v.push_back(new Derived("3"));
+    Derived* derived = new Derived("1");
+    derived->v.push_back(new Derived("2"));
+    derived->v.push_back(new Derived("4"));
+    derived->v.push_back(new Derived("3"));
+    derived->v.push_back(new Derived("1"));
 
-    Base* obj = d;
-    for(auto& value : *obj)
+    Base* base = derived;
+    for(auto& value : *base)
     {
-        std::cout << value->name() << std::endl;
+        std::cout << value.name() << std::endl;
     }
+
+    std::cout << std::distance(base->begin(), base->end()) << std::endl;
 
     return 0;
 }
